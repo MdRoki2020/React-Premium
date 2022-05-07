064288202663e49a9efcab9a081a336e2ea8349b
Md.Roki <73433502+MdRoki2020@users.noreply.github.com>
2022-05-07 11:41:14 +0600

    router dom compleate

--- react_route/src/Components/Navigationbar.js
+++ react_route/src/Components/Navigationbar.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 import { Navbar,Container,Nav,Form,FormControl,Button } from 'react-bootstrap';
+import {BrowserRouter as Router,Route,Link, Routes} from "react-router-dom";
+import About from './About';
+import Contact from './Contact';
+import Portfolio from './Portfolio';
 
 export default class Navigationbar extends Component {
   render() {
     return (
-      <div>
+      <Router>
+        <div>
         <Navbar bg="light" expand="lg">
             <Container fluid>
                 <Navbar.Brand href="#">REACT</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                 <Nav className="me-auto">
-                    <Nav.Link href="#action1">Home</Nav.Link>
-                    <Nav.Link href="#action2">Link</Nav.Link>
+                    <Nav.Link as={Link} to={'/'}> About</Nav.Link>
+                    <Nav.Link as={Link} to={'/contact'}> Contact</Nav.Link>
+                    <Nav.Link as={Link} to={'/portfolio'}> Portfolio</Nav.Link>
+                    
                 </Nav>
                 <Form className="d-flex">
                     <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
@@ -21,7 +28,14 @@ export default class Navigationbar extends Component {
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-      </div>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </Router>
     )
   }
 }
