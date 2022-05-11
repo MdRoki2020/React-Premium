f62721c6d77d2ff3b7c0e777ffd3780992de853d
MdRoki2020 <mroki815@gmail.com>
2022-05-11 23:27:17 +0600

    working on login system

--- REACT_ROUTE_WITH_MYSQL/clint/src/Components/Admin.js
+++ REACT_ROUTE_WITH_MYSQL/clint/src/Components/Admin.js
@@ -25,16 +25,18 @@ export default function Admin() {
         <form>
         <div className="form-group my-1">
           <label for="exampleInputEmail1">Email address</label>
-          <input type="email" onChange={(e)=>{setEmail(e.target.value)}} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+          <input type="text" onChange={(e)=>{setEmail(e.target.value)}} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
         </div>
         <div className="form-group">
           <label for="exampleInputPassword1">Password</label>
-          <input type="password" onChange={(e)=>{setPassword(e.target.value)}} className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+          <input type="text" onChange={(e)=>{setPassword(e.target.value)}} className="form-control" id="exampleInputPassword1" placeholder="Password"/>
         </div>
         <button type="submit" onClick={login} className="btn btn-primary form-control my-3">Login</button>
       </form>
         </div>
-        <div className='col-md-4'></div>
+        <div className='col-md-4'>
+          <h2>{email}</h2>
+        </div>
       </div>
     </div>
   )
--- REACT_ROUTE_WITH_MYSQL/server/index.js
+++ REACT_ROUTE_WITH_MYSQL/server/index.js
@@ -2,13 +2,14 @@ const express=require('express');
 const mysql=require('mysql');
 const cors=require("cors");
 
+
 const app=express();
 app.use(express.json());
 app.use(cors());
 
-//for login system..
-const bcrypt=require('bcrypt');
-const saltRounds=10;
+const bodyparser=require('body-parser');
+app.use(bodyparser.urlencoded({extended:false}));
+
 
 let con=mysql.createConnection({host:"localhost",user:"root",password:"",database:"fullstack"});
 
@@ -20,6 +21,7 @@ app.post("/insert",(req,res)=>{
     const userName=req.body.userName;
     const userPhone=req.body.userPhone;
 
+
     con.query("INSERT INTO `users`(`userName`, `userPhone`) VALUES (?,?)",[userName,userPhone]);
 }).listen(3001);
 
@@ -29,15 +31,23 @@ app.post("/login",(req,res)=>{
     const email=req.body.email;
     const password=req.body.password;
 
-    con.query("SELECT * FROM `auth` WHERE `email`=? AND `password`=?",[email,password],
-    (err,result)=>{
-        if(err){
-            res.send({err:err});
-        }
-        if(result.length>0){
-            res.send(result);
-        }else{
-            res.send({message: "wrong Email or password"});
-        }
-    })
+
+    if(email=='roki' || password=='1234'){
+         res.redirect("http://localhost:3000/dashboard");
+    }else{
+        res.end('not okey');
+    }
+
+    // con.query("SELECT * FROM `auth` WHERE `email`=? AND `password`=?",[email,password],
+    // (err,result)=>{
+    //     if(result==true){
+    //         res.send({err:err});
+    //         res.redirect('/dashboard');
+    //     }
+    //     if(result.length>0){
+    //         res.send(result);
+    //     }else{
+    //         res.send({message: "wrong Email or password"});
+    //     }
+    // })
 }).listen(3002);
