dcb3ed8c38de2bb79a0ce86aa9c3f3eda2e727af
MdRoki2020 <mroki815@gmail.com>
2022-12-24 22:04:28 +0600

    updated file upload api its now image upload

--- MERN-CRUD/server/src/utility/filehelper.js
+++ MERN-CRUD/server/src/utility/filehelper.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
     }
 });
 const filefilter = (req, file, cb) => {
-    if (file.mimetype === 'video/mkv' || file.mimetype === 'video/mp4' ){
+    if (file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/gif'){
             cb(null, true);
         }else {
             cb(null, false);
