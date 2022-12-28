71a8f05e7621cd084360efeb29ea5173040adba8
MdRoki2020 <mroki815@gmail.com>
2022-12-28 21:28:10 +0600

    yeh yeh solve solve photo fetch problem solve

--- MERN-CRUD/clint/src/components/pages/Player.jsx
+++ MERN-CRUD/clint/src/components/pages/Player.jsx
@@ -2,15 +2,14 @@ import React, { useEffect, useState } from 'react'
 // import { useNavigate } from 'react-router-dom';
 import { ReadVideo } from '../Api Request/ApiRequest';
 import '../Style/Player.css'
-import { DefaultPlayer as Video } from 'react-html5video';
+// import { DefaultPlayer as Video } from 'react-html5video';
 import 'react-html5video/dist/styles.css';
-import mainVideo from '../upload/dark_cover_page2.mp4'
+// import mainVideo from '../upload/dark_cover_page2.mp4'
 
 
 const Player = () => {
 
-    const [VideoList,setVideoList]=useState([]);
-  // let navigate = useNavigate ();
+    const [VideoList,setVideoList]=useState([]);  // let navigate = useNavigate ();
 
   useEffect(()=>{
     getVideo();
@@ -25,34 +24,22 @@ const Player = () => {
 
   return (
     <div>
-      <div class="container">
-	<div class="row">
-		<div class="col-md-8">
-			<div class="mediaPlayer img-thumbnail rounded img-fluid">
-			
-			<Video autoPlay loop muted width="320" height="240"
-          controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
-          poster="http://sourceposter.jpg"
-          onCanPlayThrough={() => {
-              // Do stuff
-          }}>
-          <source src={mainVideo} type="video/webm" />
-          <track label="English" kind="subtitles" srcLang="en" src={mainVideo} default />
-      </Video>
-
-
-
-			
+      <div className="container">
+	<div className="row">
+		<div className="col-md-4">
+			<div className="img-thumbnail rounded img-fluid">
 			</div>
 		</div>
-	<div class="col-md-4">
-		<div class="containsList">
-		<table class="table table-striped">
+	<div className="col-md-8">
+		<div className="">
+		<table className="table table-striped">
 			<tbody>
 				{
-            VideoList.map((value)=>
+            VideoList.map((value,key)=>
                 <tr key={value._id}>
-                    <td>{value.videoname}</td>
+                <td>{value.videoname}</td>
+                <td>{value.filePath}</td>
+                <td><img src={`http://localhost:5000/${value.filePath}`} height="70" className="card-img-top img-responsive" alt="img"/></td>
                 </tr>
             )
         }
