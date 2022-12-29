95ad688cba5e847e8826cc8426f21b27478bdded
MdRoki2020 <mroki815@gmail.com>
2022-12-30 00:25:13 +0600

    working on join problem

--- join/server/src/model/CommentModel.js
+++ join/server/src/model/CommentModel.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const DataSchema=mongoose.Schema({
-    PostId:{type:String},
+    PostId:{type:mongoose.Schema.Types.ObjectId},
     Comment:{type:String},
     createdDate:{type:Date,default:Date.now()}
 })
