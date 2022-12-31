cc3488e1b1ab8c98c7e7ac3d4de8c7e251fbd278
MdRoki2020 <mroki815@gmail.com>
2022-12-31 23:50:12 +0600

    4 different type graph

--- graph charts/clint/src/App.js
+++ graph charts/clint/src/App.js
@@ -1,5 +1,7 @@
-import React, { PureComponent } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { AreaChart,BarChart,Line,LineChart, Bar, XAxis,Area, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
+
 
 const data = [
   {
@@ -46,11 +48,90 @@ const data = [
   },
 ];
 
+
+
 export default function App() {
   
     return (
-      <ResponsiveContainer width="100%" aspect={3}>
-        <BarChart
+      <div className='Container fluid'>
+        <div className='row my-5'>
+
+
+          <div className='col-md-3'>
+              <ResponsiveContainer width="100%" aspect={3}>
+              <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+                barSize={20}
+              >
+                <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
+              </BarChart>
+              </ResponsiveContainer>
+          </div>
+
+
+          <div className='col-md-3'>
+              <ResponsiveContainer width="100%" height="100%">
+              <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="pv" fill="#8884d8" />
+                <Bar dataKey="uv" fill="#82ca9d" />
+              </BarChart>
+              </ResponsiveContainer>
+          </div>
+
+
+          <div className='col-md-3'>
+              <ResponsiveContainer>
+              <AreaChart
+                data={data}
+                margin={{
+                  top: 10,
+                  right: 30,
+                  left: 0,
+                  bottom: 0,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+              </AreaChart>
+              </ResponsiveContainer>
+          </div>
+
+
+          <div className='col-md-3'>
+
+          <ResponsiveContainer width="100%" height="100%">
+        <LineChart
           width={500}
           height={300}
           data={data}
@@ -60,15 +141,19 @@ export default function App() {
             left: 20,
             bottom: 5,
           }}
-          barSize={20}
         >
-          <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
-        </BarChart>
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
       </ResponsiveContainer>
+
+          </div>
+        </div>
+      </div>
     );
 }
