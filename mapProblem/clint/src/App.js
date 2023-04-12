001f132b22e1f26e5a48b7c8fc0d88415739741e
Roki <mroki815@gmail.com>
2023-04-13 01:20:46 +0600

    working on gps problem

--- mapProblem/clint/src/App.js
+++ mapProblem/clint/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Map from './pages/Map';
 import NewPlaceForm from './pages/NewPlaceForm';
+import Gps from './pages/Gps';
 
 
 
 function App() {
   return (
     <div className="">
-      <Map />
+      <Gps />
     </div>
   );
 }
