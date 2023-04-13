bf4b7983735f1bd51ded623380992c334a130145
Roki <mroki815@gmail.com>
2023-04-13 12:33:44 +0600

    updawted

--- mapProblem/clint/src/App.js
+++ mapProblem/clint/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Map from './pages/Map';
 import NewPlaceForm from './pages/NewPlaceForm';
-import Gps from './pages/Gps';
 
 
 
 function App() {
   return (
     <div className="">
-      <Gps />
+      <Map />
     </div>
   );
 }
