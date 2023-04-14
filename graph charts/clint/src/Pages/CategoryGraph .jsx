f5a424b4306f50fa05a69c2c719109c749ebe77a
Roki <mroki815@gmail.com>
2023-04-14 15:15:28 +0600

    graph problem done

---
+++ graph charts/clint/src/Pages/CategoryGraph .jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const CategoryGraph = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:5000/api/v1/categoryGraph');
+    const result = await response.json();
+    setData(result);
+  };
+
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="_id" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="count" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CategoryGraph;
